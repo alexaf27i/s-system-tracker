8c794b7186d1affbff25625732644085d800c18b
alexaf27i <70088767+alexaf27i@users.noreply.github.com>
2025-03-03 06:14:49 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SSystemTracker from './src/s-system-tracker'; // Your main component
+
+// Get the root element from index.html
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render your app
+root.render(
+  <React.StrictMode>
+    <SSystemTracker />
+  </React.StrictMode>
+);
+export default SSystemTracker; // Add this line
